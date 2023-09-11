7baacc40ea94a80fde6707588a48945766ad4cdc
zhongping.ling <zhongping.ling@razer.com>
2023-09-11 18:15:57 +0800

    修复休息全屏窗口有时无法显示在最上层

--- src/Eyeshade/MainWindow.xaml.cs
+++ src/Eyeshade/MainWindow.xaml.cs
@@ -253,23 +253,54 @@ namespace Eyeshade
             return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, trayIcon);
         }
 
-        private void ShowRestingWindow()
+        private unsafe void ShowRestingWindow()
         {
+            if (AppWindow == null) return;
+
             Root.RequestedTheme = ElementTheme.Dark;
             RestingBackground.Visibility = Visibility.Visible;
-            AppWindow.SetPresenter(Microsoft.UI.Windowing.AppWindowPresenterKind.FullScreen);
-            AppWindow.Show(true);
+
+            if (AppWindow.Presenter.Kind != Microsoft.UI.Windowing.AppWindowPresenterKind.FullScreen)
+            {
+                AppWindow.SetPresenter(Microsoft.UI.Windowing.AppWindowPresenterKind.FullScreen);
+            }
+
+            if (!AppWindow.IsVisible)
+            {
+                AppWindow.Show();
+            }
+
             // https://github.com/microsoft/microsoft-ui-xaml/issues/8562
             // MoveInZOrderAtTop/SetWindowPos does not activate a window. 
             // When a window that isn't part of the foreground process tries
             // to use SetWindowPos with HWND_TOP, Windows will not allow the
             // window to appear on top of the foreground window
-            // AppWindow.MoveInZOrderAtTop();
-            PInvoke.SetForegroundWindow(new HWND(_hWnd));
+
+            // 因为2000/XP改变了SetForegroundWindow的执行方式，不允许随便把窗口提前，
+            // 打扰用户的工作。可以用附加本线程到最前面窗口的线程，从而欺骗windows。
+            var hWndForeground = PInvoke.GetForegroundWindow();
+            var foregourndThreadId = PInvoke.GetWindowThreadProcessId(hWndForeground, null);
+            var currentThreadId = PInvoke.GetCurrentThreadId();
+            if (foregourndThreadId != currentThreadId)
+            {
+                if (foregourndThreadId != 0 && currentThreadId != 0)
+                {
+                    PInvoke.AttachThreadInput(foregourndThreadId, currentThreadId, new BOOL(true));
+                }
+                PInvoke.SetForegroundWindow(new HWND(_hWnd));
+                PInvoke.SetFocus(new HWND(_hWnd));
+                AppWindow.MoveInZOrderAtTop();
+                if (foregourndThreadId != 0 && currentThreadId != 0)
+                {
+                    PInvoke.AttachThreadInput(foregourndThreadId, currentThreadId, new BOOL(false));
+                }
+            }
         }
 
         private void CloseRestingWindow()
         {
+            if (AppWindow == null) return;
+
             Root.RequestedTheme = ElementTheme.Default;
             RestingBackground.Visibility = Visibility.Collapsed;
             AppWindow.SetPresenter(Microsoft.UI.Windowing.AppWindowPresenterKind.Default);
